b378412a8bd7167e8386557fb7c57afd04524eaf
Ian Cero (URMC Laptop) <ian_cero@urmc.rochester.edu>
2023-07-26 11:45:47 -0400

    add documentation

--- R/utils-naming.R
+++ R/utils-naming.R
@@ -1,3 +1,18 @@
+
+
+#' Append today's date to the end of a file path
+#'
+#' @param path The file path string (including file name and extension) to which the
+#' current date should be appended.
+#'
+#' @return A modified string filepath with the date added (just before the
+#' file extension at the end).
+#' @export
+#'
+#' @examples
+#'
+#' my_path <- 'my_dir/my_file.xlsx'
+#' append_date(my_path)
 append_date <- function(path) {
   filename <- tools::file_path_sans_ext(path)
   extension <- tools::file_ext(path)
